bb2f167a608e00ec36a69f453c1fa10da980d118
juan_lorza <lorza112@hotmail.com>
2023-01-26 18:58:01 -0500

    modifi index

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,20 @@ import express from 'express';
 
 dotenv.config();
 
-// import connectDb from './configdt/database';
-// import routes from './routes';
-// import configExpress from './configdt/express';
+import connectDb from './configdb/database';
+import routes from './routes';
+import configExpress from './configdb/express';
 
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
-// configExpress(app);
+configExpress(app);
 
-// connectDb();
+connectDb();
 
-// routes(app);
+routes(app);
 
 
 
